9d9337450d1521c1bd3a827f11565065e389e129
Brendan Do <brendan.do@uqconnect.edu.au>
2016-10-06 01:01:16 +0000

    set up node to read serial from /dev/ttyACM0

--- src/server/README.md
+++ src/server/README.md
@@ -1,3 +1,7 @@
+Note:
+ - When building on the RPi, `npm install` may fail. This is because of the variant of Node.js that comes preinstalled with Raspbian causing native addons to Node.js not to build. There's a workaround that can be found [here.](https://github.com/fivdi/onoff/wiki/Node.js-v0.10.29-and-native-addons-on-the-Raspberry-Pi)
+
+
 To do:
  - make sure requires are working properly
  - update packages.json to run correctly
--- src/server/main.js
+++ src/server/main.js
@@ -1,6 +1,14 @@
 //Server main. To be used on RPi.
 
-import express = require('express');
-import fs = require('fs');
-import rpio = require('rpi-gpio');
+var express = require('express');
+var fs = require('fs');
+var SerialPort = require('serialport');
 
+var port = new SerialPort('/dev/ttyACM0', {
+  baudRate: 115200,
+  parser: SerialPort.parsers.readline('\n')
+});
+
+port.on('data', function(data) {
+  console.log('Data: ' + data);
+});
