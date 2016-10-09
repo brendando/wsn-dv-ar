9b278c2b2b209603c2eb53bb3f3cb30f10f28a8f
Brendan Do <brendan.do@uqconnect.edu.au>
2016-10-09 21:36:59 +1000

    tweaked networking to work around payload max size, various bug fixes and efficiencies, splitting of processes and timers added to sensor readings to reduce power consumption

--- src/nodes/remote_node.c
+++ src/nodes/remote_node.c
@@ -35,11 +35,14 @@
 #include "board-peripherals.h"
 #include "ti-lib.h"
 #include "ieee-addr.h"
+#include "dev/serial-line.h"
+#include "dev/cc26xx-uart.h"
 
 
 #include <string.h>
 #include <stdint.h>
 #include <stdbool.h>
+#include <stdio.h>
 
 #define DEBUG DEBUG_PRINT
 #include "net/ip/uip-debug.h"
@@ -50,34 +53,35 @@
 static struct uip_udp_conn *client_conn;
 /*---------------------------------------------------------------------------*/
 static int hdc1000_humid;
-static int hdc1000_temp;
+//static int hdc1000_temp;
 static int bmp280_pressure;
-static int bmp280_temp;
+//static int bmp280_temp;
 static int tmp007_temp_obj;
 static int tmp007_temp_amb;
 static int odt3001_light;
 static char addr[17];
+static char gps_string[128] = "null";
 
-PROCESS(sensor_node_process, "Sensor node process");
-AUTOSTART_PROCESSES(&resolv_process,&sensor_node_process);
-/*---------------------------------------------------------------------------*/
-static void
-tcpip_handler(void)
-{
-  printf("Please make the server stop responding.\n\r");
-}
+PROCESS(networking_process, "Networking process");
+PROCESS(sensor_process, "Sensor process");
+PROCESS(serial_process, "Serial process");
+AUTOSTART_PROCESSES(&resolv_process,&networking_process,&sensor_process,&serial_process);
 /*---------------------------------------------------------------------------*/
 static char buf[MAX_PAYLOAD_LEN];
 static void
 timeout_handler(void)
 {
-  sprintf(buf, "{id:%s,time:%d,t_obj:%d,t_amb:%d,humid:%d,press:%d,light:%d}", 
-    addr,(int)clock_seconds(), tmp007_temp_obj, tmp007_temp_amb, hdc1000_humid, bmp280_pressure, odt3001_light);
-#if SEND_TOO_LARGE_PACKET_TO_TEST_FRAGMENTATION
-  uip_udp_packet_send(client_conn, buf, UIP_APPDATA_SIZE);
-#else /* SEND_TOO_LARGE_PACKET_TO_TEST_FRAGMENTATION */
+  int uptime = (int) clock_seconds();
+  //remove the checksum to save bytes on tx
+  int cap_gps = strlen(gps_string) - 5;
+  snprintf(buf, MAX_PAYLOAD_LEN, "{\"seq\":0,\"id\":\"%s\",\"cpu_t\":%d,\"t_obj\":%d,\"t_amb\":%d,\"humid\":%d,\"press\":%d,\"light\":%d}", 
+    addr,uptime, tmp007_temp_obj, tmp007_temp_amb, hdc1000_humid, bmp280_pressure, odt3001_light);
+  PRINTF("%s\n\r", buf);
   uip_udp_packet_send(client_conn, buf, strlen(buf));
-#endif /* SEND_TOO_LARGE_PACKET_TO_TEST_FRAGMENTATION */
+  snprintf(buf, MAX_PAYLOAD_LEN, "{\"seq\":1,\"id\":\"%s\",\"cpu_t\":%d,\"gps\":\"%.*s\"}", 
+    addr, uptime, cap_gps, gps_string);
+  PRINTF("%s\n\r", buf);
+  uip_udp_packet_send(client_conn, buf, strlen(buf));
 }
 /*---------------------------------------------------------------------------*/
 static void
@@ -150,18 +154,22 @@ set_connection_address(uip_ipaddr_t *ipaddr)
   return status;
 }
 /*---------------------------------------------------------------------------*/
-PROCESS_THREAD(sensor_node_process, ev, data)
+PROCESS_THREAD(networking_process, ev, data)
 {
   static struct etimer et;
   uip_ipaddr_t ipaddr;
 
   PROCESS_BEGIN();
-  PRINTF("UDP client process started\n\r");
+  PRINTF("Networking process started\n\r");
+  
   uint8_t ieeeaddr[8];
   ieee_addr_cpy_to(ieeeaddr, 8);
-  sprintf(addr, "%02x%02x%02x%02x%02x%02x%02x%02x", 
-    ieeeaddr[0], ieeeaddr[1], ieeeaddr[2], ieeeaddr[3],
+  //shortened ID
+  sprintf(addr, "%02x%02x%02x%02x",
     ieeeaddr[4], ieeeaddr[5], ieeeaddr[6], ieeeaddr[7]);
+  //sprintf(addr, "%02x%02x%02x%02x%02x%02x%02x%02x", 
+  //  ieeeaddr[0], ieeeaddr[1], ieeeaddr[2], ieeeaddr[3],
+  //  ieeeaddr[4], ieeeaddr[5], ieeeaddr[6], ieeeaddr[7]);
 
 #if UIP_CONF_ROUTER
   set_global_address();
@@ -191,10 +199,6 @@ PROCESS_THREAD(sensor_node_process, ev, data)
 	UIP_HTONS(client_conn->lport), UIP_HTONS(client_conn->rport));
 
   etimer_set(&et, SEND_INTERVAL);
-  SENSORS_ACTIVATE(hdc_1000_sensor);
-  SENSORS_ACTIVATE(bmp_280_sensor);
-  SENSORS_ACTIVATE(tmp_007_sensor);
-  SENSORS_ACTIVATE(opt_3001_sensor);
   
   while(1) {
     PROCESS_YIELD();
@@ -203,32 +207,82 @@ PROCESS_THREAD(sensor_node_process, ev, data)
 	  if(etimer_expired(&et)) {
       timeout_handler();
       etimer_restart(&et);
-
-	  //Check for TCPIP event
-    } else if(ev == tcpip_event) {
-      tcpip_handler();
-    //sensor checks
-    } else if(ev == sensors_event) {
-      if(data == &hdc_1000_sensor) {
-        hdc1000_humid = hdc_1000_sensor.value(HDC_1000_SENSOR_TYPE_HUMIDITY);
-        hdc1000_temp = hdc_1000_sensor.value(HDC_1000_SENSOR_TYPE_TEMP);
-        SENSORS_ACTIVATE(hdc_1000_sensor);
-      } else if(data == &bmp_280_sensor) {
-        bmp280_pressure = bmp_280_sensor.value(BMP_280_SENSOR_TYPE_PRESS);
-        bmp280_temp = bmp_280_sensor.value(BMP_280_SENSOR_TYPE_TEMP);
-        SENSORS_ACTIVATE(bmp_280_sensor);
-      } else if(data == &tmp_007_sensor) {
-        tmp_007_sensor.value(TMP_007_SENSOR_TYPE_ALL);
-        tmp007_temp_obj = tmp_007_sensor.value(TMP_007_SENSOR_TYPE_OBJECT);
-        tmp007_temp_amb = tmp_007_sensor.value(TMP_007_SENSOR_TYPE_AMBIENT);
-        SENSORS_ACTIVATE(tmp_007_sensor);
-      } else if(data == &opt_3001_sensor) {
-        odt3001_light = opt_3001_sensor.value(0);
-        SENSORS_ACTIVATE(opt_3001_sensor);
-      }
-    } 
+    }
   }
 
   PROCESS_END();
 }
 /*---------------------------------------------------------------------------*/
+PROCESS_THREAD(sensor_process, ev, data)
+{
+  static struct etimer hdc1000_t;
+  static struct etimer bmp280_t;
+  static struct etimer tmp007_t;
+  static struct etimer opt3001_t;
+
+  PROCESS_BEGIN();
+
+  SENSORS_ACTIVATE(hdc_1000_sensor);
+  SENSORS_ACTIVATE(bmp_280_sensor);
+  SENSORS_ACTIVATE(tmp_007_sensor);
+  SENSORS_ACTIVATE(opt_3001_sensor);
+  
+
+  while(1) {
+    PROCESS_YIELD();
+    if(ev == sensors_event) {
+      if(data == &hdc_1000_sensor) {
+        hdc1000_humid = hdc_1000_sensor.value(HDC_1000_SENSOR_TYPE_HUMIDITY);
+        //hdc1000_temp = hdc_1000_sensor.value(HDC_1000_SENSOR_TYPE_TEMP);
+        etimer_set(&hdc1000_t, 3 * CLOCK_SECOND);
+      } else if(data == &bmp_280_sensor) {
+        bmp280_pressure = bmp_280_sensor.value(BMP_280_SENSOR_TYPE_PRESS);
+        //bmp280_temp = bmp_280_sensor.value(BMP_280_SENSOR_TYPE_TEMP);
+        etimer_set(&bmp280_t, 3 * CLOCK_SECOND);
+      } else if(data == &tmp_007_sensor) {
+        tmp_007_sensor.value(TMP_007_SENSOR_TYPE_ALL);
+        tmp007_temp_obj = tmp_007_sensor.value(TMP_007_SENSOR_TYPE_OBJECT);
+        tmp007_temp_amb = tmp_007_sensor.value(TMP_007_SENSOR_TYPE_AMBIENT);
+        etimer_set(&tmp007_t, 3 * CLOCK_SECOND);
+      } else if(data == &opt_3001_sensor) {
+        odt3001_light = opt_3001_sensor.value(0);
+        etimer_set(&opt3001_t, 3 * CLOCK_SECOND);
+      }
+    } else if(ev == PROCESS_EVENT_TIMER) {
+      if(etimer_expired(&hdc1000_t)) {
+        SENSORS_ACTIVATE(hdc_1000_sensor);
+      }
+      if(etimer_expired(&bmp280_t)) {
+        SENSORS_ACTIVATE(bmp_280_sensor);
+      }
+      if(etimer_expired(&tmp007_t)) {
+        SENSORS_ACTIVATE(tmp_007_sensor);
+      }
+      if(etimer_expired(&opt3001_t)) {
+        SENSORS_ACTIVATE(opt_3001_sensor);
+      }
+    }
+  }
+  PROCESS_END();
+}
+/*---------------------------------------------------------------------------*/
+PROCESS_THREAD(serial_process, ev, data)
+{
+  static char gps_title[] = "$GPGGA";
+  PROCESS_BEGIN();
+
+  PRINTF("Serial process started...\n\r");
+  cc26xx_uart_set_input(serial_line_input_byte);
+
+
+  while(1) {
+    PROCESS_YIELD();
+    if(ev == serial_line_event_message) {
+      if(!memcmp(gps_title, data, 6)) {
+        PRINTF("%s\n\r", (char *) data);
+        strncpy(gps_string, (char *)data, 127);
+      }
+    }
+  }
+  PROCESS_END();
+}
